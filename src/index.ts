0de7aadcea16828625d25163e88c0e9072a02a6d
Sam <samuel.waters@gmail.com>
2023-03-01 09:04:32 -0700

    chore:functions done

--- src/index.ts
+++ src/index.ts
@@ -79,11 +79,11 @@ function greeting(name:string, relationship?:string):string {
     return name + " " + (relationship || "Super")
 }
 
-console.log(greeting("sam"))
+// console.log(greeting("sam"))
 
 //default
 function greet(name:string, relationship = "sup"):string {
     return name + " " + relationship
 }
 
-console.log(greet("sam"))
+// console.log(greet("sam"))
