b9d18f221d17e5b7b9b078f11403a3ac9e52cfe9
Sam <samuel.waters@gmail.com>
2023-02-28 19:52:39 -0700

    chore:anytype function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 // Typescript types
 // any
+let student;
+student = 1;
+student = "any type"
+
+function getStudent(anyType: any){
+    //no implicient any changed to false will remove the error below anyType without the declaration
+    console.log(anyType)
+}
 
 
 // number
@@ -8,6 +16,9 @@ let age: number = 20;
 
 //string
 let courses: string = "javascript";
+let course = "javascript";
+
 
 // boolean
 let passed: boolean = false;
+let failed = false;
